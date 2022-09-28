44af01885cbdf69eb8c83595cfb99bd600250066
Shubham <privatestartup2004@gmail.com>
2022-09-28 17:29:22 +0530

    Ready to Use

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from "axios";
 import './App.css';
+import { clear } from '@testing-library/user-event/dist/clear';
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
     if (!cityName) return
     const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
     axios.get(apiURL).then((res) => {
-      console.log("Response", res.data)
+      // console.log("Response", res.data)
 
       setData(res.data)
     }).catch((err) => {
@@ -34,7 +35,7 @@ function App() {
           <h1 className='heading text-center'>Weather App</h1>
 
           <div className="buttons d-grid gap-3 col-4 mt-4">
-            <input type="text" className="form-control" onChange={handleChangeInput} />
+            <input type="text" className="form-control text-center" placeholder='City Name' onChange={handleChangeInput} />
             <button className='btn btn-info' type='button' onClick={handleSearch}>Search</button>
           </div>
         </div>
@@ -42,8 +43,21 @@ function App() {
           <div className="shadow-lg p-1 mb-5 bg-body rounded weatherResultBox">
             <img src="https://png.pngitem.com/pimgs/s/399-3999742_weather-icon-png-image-weather-app-icon-transparent.png" alt="Weather" />
             <h5 className='weatherCity'>{data?.name}</h5>
-            <h6 className='weatherTemp'>{((data?.main?.temp) - 273.15).toFixed(2)}°C</h6>
+            <h6 className='weatherTemp'>{((data?.main?.temp) - 273.15).toFixed(0)}°C</h6><br />
+            <div className="item">
+              <p className='weatherItem'>Humidity</p>
+              <p className='weatherItem'>Air Pressure</p>
+              <p className='weatherItem'>Wind Speed</p>
+            </div>
+            <div className="item">
+              <p className='weatherItem mx-4'>{((data?.main?.humidity))}%</p>
+              <p className='weatherItem mx-2'>{((data?.main?.pressure))} hPa</p>
+              <p className='weatherItem mx-2'>{((data?.wind?.speed))} km/h</p>
+            </div>
           </div>
+        </div><br /><br /><br /><br />
+        <div className='foot text-center'>
+          <h6>Copyright &copy; Weather App || All Rights Reserved 2004-2022</h6>
         </div>
       </div>
     </>
